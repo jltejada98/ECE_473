659197c1e2ee549d6947f66783f96c1ef5228931
jltejada98 <tejada@purdue.edu>
2021-03-01 17:39:41 -0500

    Completed Naive Bayes (Achieved > 0.8 Accuracy on Digits Dataset)

--- HW3/hw3_submission_sol.py
+++ HW3/hw3_submission_sol.py
@@ -44,9 +44,9 @@ class Gaussian_Naive_Bayes():
         # hint : to see all unique y labels, you might use np.unique function, e.g., np.unique(self.y)
 
         unique, counts = np.unique(self.y, return_counts=True)
-        classIndex = 0
         self.y_prior = np.zeros((1, len(unique)))
         numSamples = np.sum(counts)
+        classIndex = 0
         while (classIndex < len(unique)):
             self.y_prior[0][classIndex] = counts[classIndex] / numSamples
             classIndex += 1
@@ -63,6 +63,7 @@ class Gaussian_Naive_Bayes():
         for key in self.x_by_class:
             self.mean_by_class[key] = self.mean(self.x_by_class[key])
             self.std_by_class[key] = self.std(self.x_by_class[key])
+
 
         # END_YOUR_CODE
         ############################################################
@@ -90,10 +91,8 @@ class Gaussian_Naive_Bayes():
         mean = self.mean(x)
         sample_minus_mean = x-mean
         square_difference = sample_minus_mean**2
-        summation = np.sum(square_difference, axis=0)
-        std = np.sqrt((1.0/(len(x)-1))*summation) + 1e-7
-
-
+        summation = np.sum(square_difference, axis=0) + 1E-7
+        std = np.sqrt((1.0/(len(x)-1))*(summation))
         # END_YOUR_CODE
         ############################################################
         ############################################################
@@ -133,7 +132,7 @@ class Gaussian_Naive_Bayes():
 
         for sample in range(0,n):
             for c in range(0, num_class):
-                prediction[sample][c] =  self.y_prior[0][c] + np.sum(np.log(self.calc_gaussian_dist(x[sample], self.mean_by_class[c], self.std_by_class[c])+ 1e-7))
+                prediction[sample][c] =  self.y_prior[0][c] + np.sum(np.log(self.calc_gaussian_dist(x[sample], self.mean_by_class[c], self.std_by_class[c])+ + 1E-20))
 
 
         # END_YOUR_CODE
@@ -142,175 +141,175 @@ class Gaussian_Naive_Bayes():
         
         return prediction
 
-#
-# class Neural_Network():
-#     def __init__(self, hidden_size = 64, output_size = 1):
-#         self.W1 = None
-#         self.b1 = None
-#         self.W2 = None
-#         self.b2 = None
-#
-#         self.hidden_size = hidden_size
-#         self.output_size = output_size
-#
-#     def fit(self, x, y, batch_size = 64, iteration = 2000, learning_rate = 1e-3):
-#         """
-#         Train this 2 layered neural network classifier using mini-batch stochastic gradient descent.
-#         Inputs:
-#         - X: A numpy array of shape (N, D) containing training data; there are N
-#           training samples each of dimension D.
-#         - y: A numpy array of shape (N,) containing training labels; y[i] = c
-#           means that X[i] has label 0 <= c < C for C classes.
-#         - learning_rate: (float) learning rate for optimization.
-#         - iteration: (integer) number of steps to take when optimizing
-#         - batch_size: (integer) number of training examples to use at each step.
-#
-#         Use the given learning_rate, iteration, or batch_size for this homework problem.
-#
-#         Returns:
-#         None
-#         """
-#         dim = x.shape[1]
-#         num_train = x.shape[0]
-#
-#         #initialize W
-#         if self.W1 == None:
-#             self.W1 = 0.001 * np.random.randn(dim, self.hidden_size)
-#             self.b1 = 0
-#
-#             self.W2 = 0.001 * np.random.randn(self.hidden_size, self.output_size)
-#             self.b2 = 0
-#
-#
-#         for it in range(iteration):
-#             batch_ind = np.random.choice(num_train, batch_size)
-#
-#             x_batch = x[batch_ind]
-#             y_batch = y[batch_ind]
-#
-#             loss, gradient = self.loss(x_batch, y_batch)
-#
-#             ############################################################
-#             ############################################################
-#             # BEGIN_YOUR_CODE
-#             # Update parameters with mini-batch stochastic gradient descent method
-#
-#             pass;
-#
-#             # END_YOUR_CODE
-#             ############################################################
-#             ############################################################
-#
-#             y_pred = self.predict(x_batch)
-#             acc = np.mean(y_pred == y_batch)
-#
-#             if it % 50 == 0:
-#                 print('iteration %d / %d: accuracy : %f: loss : %f' % (it, iteration, acc, loss))
-#
-#     def loss(self, x_batch, y_batch, reg = 1e-3):
-#             """
-#             Implement feed-forward computation to calculate the loss function.
-#             And then compute corresponding back-propagation to get the derivatives.
-#
-#             Inputs:
-#             - X_batch: A numpy array of shape (N, D) containing a minibatch of N
-#               data points; each point has dimension D.
-#             - y_batch: A numpy array of shape (N,) containing labels for the minibatch.
-#             - reg: hyperparameter which is weight of the regularizer.
-#
-#             Returns: A tuple containing:
-#             - loss as a single float
-#             - gradient dictionary with four keys : 'dW1', 'db1', 'dW2', and 'db2'
-#             """
-#             gradient = {'dW1' : None, 'db1' : None, 'dW2' : None, 'db2' : None}
-#
-#
-#             ############################################################
-#             ############################################################
-#             # BEGIN_YOUR_CODE
-#             # Calculate y_hat which is probability of the instance is y = 0.
-#
-#             pass;
-#
-#             # END_YOUR_CODE
-#             ############################################################
-#             ############################################################
-#
-#
-#             ############################################################
-#             ############################################################
-#             # BEGIN_YOUR_CODE
-#             # Calculate loss and gradient
-#
-#             pass;
-#
-#             # END_YOUR_CODE
-#             ############################################################
-#             ############################################################
-#             return loss, gradient
-#
-#     def activation(self, z):
-#         """
-#         Compute the ReLU output of z
-#         Inputs:
-#         z : A scalar or numpy array of any size.
-#         Return:
-#         s : output of ReLU(z)
-#         """
-#         ############################################################
-#         ############################################################
-#         # BEGIN_YOUR_CODE
-#         # Implement ReLU
-#
-#         pass;
-#
-#         # END_YOUR_CODE
-#         ############################################################
-#         ############################################################
-#
-#         return s
-#
-#     def sigmoid(self, z):
-#         """
-#         Compute the sigmoid of z
-#         Inputs:
-#         z : A scalar or numpy array of any size.
-#         Return:
-#         s : sigmoid of input
-#         """
-#         ############################################################
-#         ############################################################
-#         # BEGIN_YOUR_CODE
-#
-#         pass;
-#
-#         # END_YOUR_CODE
-#         ############################################################
-#         ############################################################
-#
-#         return s
-#
-#     def predict(self, x):
-#         """
-#         Use the trained weights of this linear classifier to predict labels for
-#         data points.
-#         Inputs:
-#
-#         Returns:
-#         - y_pred: Predicted labels for the data in X. y_pred is a 1-dimensional
-#           array of length N, and each element is an integer giving the predicted
-#           class.
-#         """
-#         ############################################################
-#         ############################################################
-#         # BEGIN_YOUR_CODE
-#         # Calculate predicted y
-#
-#         pass;
-#
-#         # END_YOUR_CODE
-#         ############################################################
-#         ############################################################
-#         return y_hat
-#
+
+class Neural_Network():
+    def __init__(self, hidden_size = 64, output_size = 1):
+        self.W1 = None
+        self.b1 = None
+        self.W2 = None
+        self.b2 = None
+
+        self.hidden_size = hidden_size
+        self.output_size = output_size
+
+    def fit(self, x, y, batch_size = 64, iteration = 2000, learning_rate = 1e-3):
+        """
+        Train this 2 layered neural network classifier using mini-batch stochastic gradient descent.
+        Inputs:
+        - X: A numpy array of shape (N, D) containing training data; there are N
+          training samples each of dimension D.
+        - y: A numpy array of shape (N,) containing training labels; y[i] = c
+          means that X[i] has label 0 <= c < C for C classes.
+        - learning_rate: (float) learning rate for optimization.
+        - iteration: (integer) number of steps to take when optimizing
+        - batch_size: (integer) number of training examples to use at each step.
+
+        Use the given learning_rate, iteration, or batch_size for this homework problem.
+
+        Returns:
+        None
+        """
+        dim = x.shape[1]
+        num_train = x.shape[0]
+
+        #initialize W
+        if self.W1 == None:
+            self.W1 = 0.001 * np.random.randn(dim, self.hidden_size)
+            self.b1 = 0
+
+            self.W2 = 0.001 * np.random.randn(self.hidden_size, self.output_size)
+            self.b2 = 0
+
+
+        for it in range(iteration):
+            batch_ind = np.random.choice(num_train, batch_size)
+
+            x_batch = x[batch_ind]
+            y_batch = y[batch_ind]
+
+            loss, gradient = self.loss(x_batch, y_batch)
+
+            ############################################################
+            ############################################################
+            # BEGIN_YOUR_CODE
+            # Update parameters with mini-batch stochastic gradient descent method
+
+            pass;
+
+            # END_YOUR_CODE
+            ############################################################
+            ############################################################
+
+            y_pred = self.predict(x_batch)
+            acc = np.mean(y_pred == y_batch)
+
+            if it % 50 == 0:
+                print('iteration %d / %d: accuracy : %f: loss : %f' % (it, iteration, acc, loss))
+
+    def loss(self, x_batch, y_batch, reg = 1e-3):
+            """
+            Implement feed-forward computation to calculate the loss function.
+            And then compute corresponding back-propagation to get the derivatives.
+
+            Inputs:
+            - X_batch: A numpy array of shape (N, D) containing a minibatch of N
+              data points; each point has dimension D.
+            - y_batch: A numpy array of shape (N,) containing labels for the minibatch.
+            - reg: hyperparameter which is weight of the regularizer.
+
+            Returns: A tuple containing:
+            - loss as a single float
+            - gradient dictionary with four keys : 'dW1', 'db1', 'dW2', and 'db2'
+            """
+            gradient = {'dW1' : None, 'db1' : None, 'dW2' : None, 'db2' : None}
+
+
+            ############################################################
+            ############################################################
+            # BEGIN_YOUR_CODE
+            # Calculate y_hat which is probability of the instance is y = 0.
+
+            pass;
+
+            # END_YOUR_CODE
+            ############################################################
+            ############################################################
+
+
+            ############################################################
+            ############################################################
+            # BEGIN_YOUR_CODE
+            # Calculate loss and gradient
+
+            pass;
+
+            # END_YOUR_CODE
+            ############################################################
+            ############################################################
+            return loss, gradient
+
+    def activation(self, z):
+        """
+        Compute the ReLU output of z
+        Inputs:
+        z : A scalar or numpy array of any size.
+        Return:
+        s : output of ReLU(z)
+        """
+        ############################################################
+        ############################################################
+        # BEGIN_YOUR_CODE
+        # Implement ReLU
+
+        pass;
+
+        # END_YOUR_CODE
+        ############################################################
+        ############################################################
+
+        return s
+
+    def sigmoid(self, z):
+        """
+        Compute the sigmoid of z
+        Inputs:
+        z : A scalar or numpy array of any size.
+        Return:
+        s : sigmoid of input
+        """
+        ############################################################
+        ############################################################
+        # BEGIN_YOUR_CODE
+
+        pass;
+
+        # END_YOUR_CODE
+        ############################################################
+        ############################################################
+
+        return s
+
+    def predict(self, x):
+        """
+        Use the trained weights of this linear classifier to predict labels for
+        data points.
+        Inputs:
+
+        Returns:
+        - y_pred: Predicted labels for the data in X. y_pred is a 1-dimensional
+          array of length N, and each element is an integer giving the predicted
+          class.
+        """
+        ############################################################
+        ############################################################
+        # BEGIN_YOUR_CODE
+        # Calculate predicted y
+
+        pass;
+
+        # END_YOUR_CODE
+        ############################################################
+        ############################################################
+        return y_hat
+
     
